78deaeda4ad2e094e69ff14d24e58bf6cede1f91
SANRUX-S <sanruxsxx@gmail.com>
2026-09-18 18:52:05 +0530

    Load route page styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './home-reference.css'
+import './pages.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>)
